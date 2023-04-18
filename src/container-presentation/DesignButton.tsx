64c32b8012073ed0392b796518f7990e0277696a
jhkim06 <jhkim06@miridih.com>
2023-04-18 11:07:48 +0900

    Presentational Container Component Pattern With React

---
+++ src/container-presentation/DesignButton.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+interface DesignButtonProps {
+  look: ThemeLook;
+  buttonProps: {
+    text: string;
+    onClick: () => void;
+  };
+}
+
+export function DesignButton({ look, buttonProps }: DesignButtonProps) {
+  <button onClick={buttonProps.onClick}>{buttonProps.text}</button>;
+}
+
+const ThemeButton = styled.button<{ look: ThemeLook }>`
+  position: relative;
+  cursor: ${({ look }) => look.pointer && "pointer"};
+  color: ${({ look }) => look.color};
+  ...
+`;
